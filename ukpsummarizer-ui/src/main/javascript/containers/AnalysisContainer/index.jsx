2a6f6c44f3272133dbbc1fce0ae9e8dd06bd8f38
Avinesh Polisetty Venkata Sai <avinesh@aiphes.tu-darmstadt.de>
2018-10-31 18:46:16 +0100

    An updated version but broken

--- ukpsummarizer-ui/src/main/javascript/containers/AnalysisContainer/index.jsx
+++ ukpsummarizer-ui/src/main/javascript/containers/AnalysisContainer/index.jsx
@@ -462,12 +462,8 @@ class AnalysisContainer extends React.Component {
         return (<Grid>
                 <Row>
                     <Col xs={12}>
-                        <p className="lead">
-                        <small><b>Topic: </b><em>{title}</em></small><br/>
-                        <Well>
-                                <b>Query: </b>{narrative}
-                        </Well>
-                        </p>
+                        <PageHeader> <b>Topic: </b><em>{title}</em> </PageHeader>
+                        <Well><b>Query: </b>{narrative}</Well>
                     </Col>
                 </Row>
                 <Row>
@@ -492,6 +488,7 @@ class AnalysisContainer extends React.Component {
                                 <Json json={latestIteration.weights}/>
                             </Tab>
                         </Tabs>
+
                         <h4>Legend:</h4>
                         <Table>
                             <thead>
